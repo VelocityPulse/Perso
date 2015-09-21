f0237e5d3502e937f64ab994c7783faf88e83a54
VelocityPulse <VelocityPulse@outlook.fr>
2015-09-21 18:07:48 +0200

    up

---
+++ Utilitaire/ft_put.h
@@ -0,0 +1,12 @@
+#ifndef FT_PUT_H
+# define FT_PUT_H
+
+#include <unistd.h>
+
+void    ft_putchar(char c);
+void    ft_putstr(char *str);
+void    ft_putnbr(int n);
+void    ft_putnbr_un(unsigned int n);
+char    *ft_strcpy(char *src, char *dest);
+
+#endif // FT_PUT_H
