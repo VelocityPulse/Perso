16747b4ca00a74ac02a6c956d73776d3cc1ff38f
VelocityPulse <VelocityPulse@outlook.fr>
2015-10-21 23:33:44 +0200

    Ajout d'un thread et amélioration du code

--- Mathias Spamer/main.cpp
+++ Mathias Spamer/main.cpp
@@ -3,10 +3,49 @@
 
 HHOOK _hook;
 KBDLLHOOKSTRUCT kbdStruct;
+bool active;
+
+DWORD WINAPI spammer(void*)
+{
+	while (active == 1)
+	{
+		Sleep(100); // ici vous pouvez gérer la vitesse du spam. ( 1000 pour 1s )
+
+		keybd_event(0x39, 0, 0, 0);							// ç
+		keybd_event(0x39, 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event('A', 0, 0, 0);							// a
+		keybd_event('A', 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event(VK_SPACE, 0, 0, 0);						// space
+		keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event('V', 0, 0, 0);							// v
+		keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event('A', 0, 0, 0);							// a
+		keybd_event('A', 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event(VK_SPACE, 0, 0, 0);						// space
+		keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event(VK_SHIFT, 0, 0, 0); // shift
+
+		keybd_event(188, 0, 0, 0);							// ? 
+		keybd_event(188, 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+
+		keybd_event(VK_RETURN, 0, 0, 0);					// entrer
+		keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+	}
+	return 0;
+}
+
 
 LRESULT __stdcall HookProcedure(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	static bool active;
+
 	if (nCode >= 0)
 	{
 		if (wParam == WM_KEYDOWN)
@@ -14,42 +53,10 @@ LRESULT __stdcall HookProcedure(int nCode, WPARAM wParam, LPARAM lParam)
 			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
 			if (kbdStruct.vkCode == VK_F2)
 			{
-				//MessageBox(NULL, L"F2 is pressed!", L"key pressed", MB_ICONINFORMATION);
 				if (active == 0)
 				{
 					active = 1;
-					while (active = 1)
-					{
-						Sleep(50);
-						
-						keybd_event(0x39, 0, 0, 0);							// ç
-						keybd_event(0x39, 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event('A', 0, 0, 0);							// a
-						keybd_event('A', 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event(VK_SPACE, 0, 0, 0);						// space
-						keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event('V', 0, 0, 0);							// v
-						keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event('A', 0, 0, 0);							// a
-						keybd_event('A', 0, KEYEVENTF_KEYUP, 0);
-						
-						keybd_event(VK_SPACE, 0, 0, 0);						// space
-						keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event(VK_SHIFT, 0, 0, 0); // shift
-
-						keybd_event(188, 0, 0, 0);							// ? 
-						keybd_event(188, 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
-
-						keybd_event(VK_RETURN, 0, 0, 0);					// entrer
-						keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
-					}
+					CreateThread(0, 0, spammer, 0, 0, 0);
 				}
 				else
 					active = 0;
@@ -63,7 +70,7 @@ void SetHook()
 {
 	if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookProcedure, NULL, 0)))
 	{
-		MessageBox(NULL, L"Failed to install hook!", L"Error", MB_ICONERROR);
+		MessageBox(NULL, "Failed to install hook!", "Error", MB_ICONERROR);
 	}
 }
 
